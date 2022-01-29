0ef62b6de2122258e6d3f7841d131426c0a8bdf1
gabrielhuluany <gabrielhuluany@outlook.com>
2022-01-29 00:22:16 -0300

    Update Serie.cs

--- Classes/Serie.cs
+++ Classes/Serie.cs
@@ -3,13 +3,9 @@ namespace DIO.cadastroSeries
     public class Serie : EntidadeBase
     {
         // Atributos
-
         private Genero Genero { get; set; }
-
         private string Titulo { get; set; }
-
         private string Descricao { get; set; }
-
         private int Ano { get; set; }
     }
 }
