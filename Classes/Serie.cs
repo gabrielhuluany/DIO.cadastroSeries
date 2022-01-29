deda11edcd4cd231ce407f06e8fb592d730c121a
gabrielhuluany <gabrielhuluany@outlook.com>
2022-01-29 00:29:35 -0300

    Update Serie.cs

--- Classes/Serie.cs
+++ Classes/Serie.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace DIO.cadastroSeries
 {
     public class Serie : EntidadeBase
@@ -16,6 +18,17 @@ namespace DIO.cadastroSeries
 			this.Titulo = titulo;
 			this.Descricao = descricao;
 			this.Ano = ano;
+		}
+
+        public override string ToString()
+		{
+			// Environment.NewLine https://docs.microsoft.com/en-us/dotnet/api/system.environment.newline?view=netcore-3.1
+            string retorno = "";
+            retorno += "Gênero: " + this.Genero + Environment.NewLine;
+            retorno += "Titulo: " + this.Titulo + Environment.NewLine;
+            retorno += "Descrição: " + this.Descricao + Environment.NewLine;
+            retorno += "Ano de Início: " + this.Ano + Environment.NewLine;
+			return retorno;
 		}
     }
 }
