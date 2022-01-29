66d045227674c4b219e3bf880bfa54bdb77409cc
gabrielhuluany <gabrielhuluany@outlook.com>
2022-01-29 00:48:26 -0300

    Update SerieRepositorio.cs

--- Classes/SerieRepositorio.cs
+++ Classes/SerieRepositorio.cs
@@ -1,7 +1,39 @@
+using System;
+using System.Collections.Generic;
+using DIO.cadastroSeries.Interfaces;
+
 namespace DIO.cadastroSeries
 {
-    public class SerieRepositorio
+    public class SerieRepositorio : IRepositorio<Serie>
     {
-        
+        public void Atualiza(int id, Serie entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Exclui(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insere(Serie entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Serie> Lista()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ProximoId()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Serie RetornaPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
